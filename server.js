c34422d5d1e978da04f1d28d5608f5bd99d980a4
lgfahys <lindsey@codeasite.com>
2019-06-14 15:35:01 -0400

    setup wireframe and routes

---
+++ server.js
@@ -0,0 +1,18 @@
+// Dependencies
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+require(path.join(__dirname, './routes/htmlRoutes'))(app);
+
+app.listen(PORT, function() {
+	console.log("App listening on PORT: " + PORT);
+});
